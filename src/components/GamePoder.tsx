237991b3b4c8eb097529d708e3c43eda6db8e443
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2024-11-26 18:00:59 -0500

    Agregar componente GamePoder con lógica de preguntas y respuestas; incluir modal de resultados y estilos de tarjeta

--- src/components/GamePoder.tsx
+++ src/components/GamePoder.tsx
@@ -1,29 +1,111 @@
 import { questionnaire } from "../data/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./GamePoder.css";
+import Modal from "./Modal";
 
 const GamePoder = () => {
   const [questions, setQuestions] = useState(questionnaire);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClickArticle = (id: string) => {
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === id) {
+        return {
+          ...question,
+          isActive: true,
+        };
+      }
+      return {
+        ...question,
+        isActive: false,
+      };
+    });
+    setQuestions(updatedQuestions);
+  };
+
+  const handleresponse = (e: React.MouseEvent<HTMLButtonElement>, id: string, response: boolean) => {
+    // prevenir el click en los padres
+    e.stopPropagation();
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === id) {
+        console.log({
+          ...question,
+          isAnswered: true,
+        });
+        return {
+          ...question,
+          isAnswered: true,
+        };
+      }
+      return question;
+    });
+
+    setQuestions(updatedQuestions);
+
+    const question = updatedQuestions.find((question) => question.id === id);
+    if (question) {
+      if (question.correctAnswer === response) {
+        setTitle("¡Correcto! ¡Has acertado!");
+        setMessage("🎉");
+      } else {
+        setTitle("¡Incorrecto! ¡Inténtalo de nuevo!");
+        setMessage("😔");
+      }
+      setIsModalOpen(true);
+    }
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
-    <section className="max-w-7xl mx-auto px-4 pb-4 md:px-8 md:pb-8">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">
-        {questions.map((question) => (
-          <article
-            key={question.id}
-            className="bg-white shadow-lg rounded-lg p-4"
-          >
-            <h2 className="text-lg font-bold mb-2">{question.question}</h2>
-            <div className="flex justify-between">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Verdadero
-              </button>
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                Falso
-              </button>
-            </div>
-          </article>
-        ))}
-      </div>
-    </section>
+    <>
+      <section className="max-w-7xl mx-auto px-4 pb-4 md:px-8 md:pb-8 font-brandon">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-8">
+          {questions.map((question) => (
+            <article
+              key={question.id}
+              className="flip-card text-white rounded-lg cursor-pointer"
+              onClick={() => handleClickArticle(question.id)}
+            >
+              <div
+                className={`flip-card-container ${question.isActive || question.isAnswered ? "active" : "noActive"}`}
+              >
+                <div className="bg-rosado-wh-abbott front p-20 grid place-content-center">
+                  <h2 className="text-9xl font-bold">{question.id}</h2>
+                </div>
+                <div className="bg-rosado-wh-abbott back p-4 flex flex-col justify-between gap-2">
+                  <div className="flex-1 grid place-content-center">
+                    <h3 className="text-3xl text-pretty">
+                      {question.question}
+                    </h3>
+                  </div>
+                  <div className="options text-3xl space-x-4">
+                    <button className="bg-morado-wh-abbott text-white px-4 py-2 rounded-full"
+                      onClick={(e) => handleresponse(e, question.id, true)}
+                    >
+                      Verdadero
+                    </button>
+                    <button className="bg-white text-morado-wh-abbott px-4 py-2 rounded-full"
+                      onClick={(e) => handleresponse(e, question.id, false)}
+                    >
+                      Falso
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </section>
+
+      {
+        isModalOpen && <Modal title={title} message={message} closeModal={closeModal} />
+      }
+    </>
   );
 };
 
--- src/data/data.ts
+++ src/data/data.ts
@@ -2,37 +2,57 @@ interface Question {
   id: string;
   question: string;
   correctAnswer: boolean;
+  isActive: boolean;
+  isAnswered: boolean;
 }
 
 export const questionnaire: Question[] = [
   {
-      id: "1",
-      question: "Los anticonceptivos orales combinados contienen dos tipos de hormonas llamadas estrógenos y progestágenos",
-      correctAnswer: true
+    id: "1",
+    question:
+      "Los anticonceptivos orales combinados contienen dos tipos de hormonas llamadas estrógenos y progestágenos",
+    correctAnswer: true,
+    isActive: false,
+    isAnswered: false,
   },
   {
-      id: "2",
-      question: "Todas las mujeres deben tomarse un descanso después de usar anticonceptivos orales combinados durante cierto tiempo",
-      correctAnswer: false
+    id: "2",
+    question:
+      "Todas las mujeres deben tomarse un descanso después de usar anticonceptivos orales combinados durante cierto tiempo",
+    correctAnswer: false,
+    isActive: false,
+    isAnswered: false,
   },
   {
-      id: "3",
-      question: "Los anticonceptivos orales combinados hacen que las mujeres aumenten de peso",
-      correctAnswer: false
+    id: "3",
+    question:
+      "Los anticonceptivos orales combinados hacen que las mujeres aumenten de peso",
+    correctAnswer: false,
+    isActive: false,
+    isAnswered: false,
   },
   {
-      id: "4",
-      question: "El uso de un anticonceptivo oral combinado puede ayudar a mejorar los síntomas emocionales relacionados con el ciclo menstrual",
-      correctAnswer: true
+    id: "4",
+    question:
+      "El uso de un anticonceptivo oral combinado puede ayudar a mejorar los síntomas emocionales relacionados con el ciclo menstrual",
+    correctAnswer: true,
+    isActive: false,
+    isAnswered: false,
   },
   {
-      id: "5",
-      question: "Algunos anticonceptivos orales combinados se utilizan como tratamiento del acné",
-      correctAnswer: true
+    id: "5",
+    question:
+      "Algunos anticonceptivos orales combinados se utilizan como tratamiento del acné",
+    correctAnswer: true,
+    isActive: false,
+    isAnswered: false,
   },
   {
-      id: "6",
-      question: "Las mujeres mayores de 35 años no pueden usar anticonceptivos orales combinados",
-      correctAnswer: false
-  }
+    id: "6",
+    question:
+      "Las mujeres mayores de 35 años no pueden usar anticonceptivos orales combinados",
+    correctAnswer: false,
+    isActive: false,
+    isAnswered: false,
+  },
 ];
