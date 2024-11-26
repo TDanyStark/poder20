28bda25e7563260602d8e6a60b68345a03fd00a1
Stark Industries <97692264+TDanyStark@users.noreply.github.com>
2024-11-25 19:06:48 -0500

    Actualizar README, ajustar URL_BASE, agregar secciones de Hero, Goal y Brands, y configurar Tailwind en el proyecto "Poder 20"

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const URL_BASE = 'http://localhost:3000';
+export const URL_BASE = '/apps/abbott/poder20';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,13 @@ export default {
 				'rosado-wh-abbott': '#d787b5',
 				'morado-wh-abbott': '#772583',
 			},
+			backgroundImage: {
+        'abbott-gradient-wh': 'linear-gradient(45deg, #5b2094, #a950a5 55%, #e99ab0);',
+      },
+      fontFamily: {
+        georgia : ['Georgia', 'ui-serif'],
+        brandon: ['Brandon', 'ui-serif']
+      }
 		},
 	},
 	plugins: [],
